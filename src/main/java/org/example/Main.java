0fdc131138ab92ad30887fc213cd09b504dc73a1
Natalya Boichenko <natalya.andriychuk@gmail.com>
2024-04-21 11:19:58 +0300

    add uml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,56 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
+        //find Fibonacci number - O(n)
+        //get Fibonacci number - O(1)
         int iterative = findFibonacciNumberIterative(6);
-        System.out.println("Iterative O(n): " + iterative);
+        System.out.println("Iterative: " + iterative);
 
+        // find Fibonacci number - O(2^n)
         int recursive = findFibonacciNumberRecursive(6);
-        System.out.println("Recursive O(n): " + recursive);
+        System.out.println("Recursive : " + recursive);
 
+
+        //find Fibonacci number - O(n)
+        //get Fibonacci number - O(1)
         int dynProgramming = findFibonacciNumberDP(6);
-        System.out.println("DP O(n): " + dynProgramming);
+        System.out.println("DP: " + dynProgramming);
     }
 
-    private static int findFibonacciNumberIterative(int n){
-        int[] fibNumbers = new int[n];
-        fibNumbers[0] = 1;
-        fibNumbers[1] = 1;
+    private static int findFibonacciNumberIterative(int n) {
+        List<Integer> fibNumbers = new ArrayList<>();
+        fibNumbers.add(1);
+        fibNumbers.add(1);
 
         for (int i = 2; i < n; i++) {
-            fibNumbers[i] = fibNumbers[i - 1] + fibNumbers[i - 2];
+            fibNumbers.add(fibNumbers.get(i - 1) + fibNumbers.get(i - 2));
         }
-        return fibNumbers[n - 1];
+
+        return fibNumbers.get(n - 1);
     }
 
-    private static int findFibonacciNumberRecursive(int n){
+    private static int findFibonacciNumberRecursive(int n) {
         if (n <= 1) {
             return n;
         }
         return findFibonacciNumberRecursive(n - 1) + findFibonacciNumberRecursive(n - 2);
     }
 
-    private static int findFibonacciNumberDP(int n){
+    private static int findFibonacciNumberDP(int n) {
         Map<Integer, Integer> savedFibonacci = new HashMap<>();
 
-        if (n <= 1){
+        if (n <= 1) {
             return n;
         }
 
-        if (savedFibonacci.containsKey(n)){
+        if (savedFibonacci.containsKey(n)) {
             return savedFibonacci.get(n);
         }
 
